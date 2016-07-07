b24a7d791e835a17a1d89aa4451d2388637816f1
Jesse Yates <jesse.k.yates@gmail.com>
2016-07-07 08:40:38 -0700

    Working on implementing drill

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 
 ### Requirements
  1. https://rvm.io/
+ 2. Download a release of spark into ext/ and connect the symlink. (Recommended)[http://www.apache.org/dyn/closer.lua/spark/spark-1.6.2/spark-1.6.2-bin-hadoop2.6.tgz]
--- lib/components/drill.rb
+++ lib/components/drill.rb
@@ -2,8 +2,21 @@
 require 'components/base_component'
 
 class Drill < BaseComponent
+
+  DRILL_LOCAL = "io.fineo.read.drill.e2e.DrillLocalClusterE2E"
+
   def initialize
     super('DRILL_READ_HOME')
   end
 
+  def read(org, metric)
+    @org = org
+    @metric = metric
+    self
+  end
+
+  def from(batch_output_dir)
+    # wait for input
+    `read`
+  end
 end
