306a415111fbc213d0ff7669d14195d7d850a851
Jesse Yates <jesse@fineo.io>
2016-09-23 10:04:20 -0700

    Adding util to create api key

---
+++ external-e2e/lib/creating-key.rb
@@ -0,0 +1,31 @@
+
+module Fineo
+  module E2E
+  end
+end
+
+require 'optparse'
+require 'ostruct'
+
+module Fineo::E2E::CreatingKey
+
+  def parse(args, name, info)
+    options = OpenStruct.new(plans: [])
+    OptionParser.new do |opts|
+      opts.banner = "Usage: #{name} [options]"
+      opts.separator info
+
+      opts.on('-c', '--credentials FILE', "Location of the credentials FILE to use.") do |s|
+        options.credentials = s
+      end
+
+      opts.on_tail("-h", "--help", "Show this message") do
+        puts opts
+        exit
+      end
+
+    end.parse!(args)
+
+    options
+  end
+end
